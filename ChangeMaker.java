410739c6d386d373379c776d222cf60dd5611f08
maknp6 <mikekocis@gmail.com>
2019-02-01 13:24:46 -0600

    update test

--- ChangeMaker.java
+++ ChangeMaker.java
@@ -3,7 +3,7 @@ package ChangeMaker;
 import java.util.Scanner;
 
 public class ChangeMaker {
-
+//****************************************************updated********************
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
