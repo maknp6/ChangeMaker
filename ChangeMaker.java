e67216ba0f15d67cdd42cbd47f42aca12aad3b17
maknp6 <mikekocis@gmail.com>
2019-02-11 15:28:44 -0600

    change Maker

--- ChangeMaker.java
+++ ChangeMaker.java
@@ -2,79 +2,76 @@ package ChangeMaker;
 
 import java.util.Scanner;
 
-public class ChangeMaker {
-//****************************************************updated********************
-	public static void main(String[] args) {
+public class ChangeMaker 
+{
+
+	public static void main(String[] args) 
+	{
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("This program determines the amount of quarters, dimes, nickles, and pennies that will be returned to a");
-		System.out.println("customer after a customer makes a purchase.");
+	
+		System.out.println("Enter the amount of change due");
 		System.out.println();
-		System.out.println("Please enter the change due to the customer (Please note, change should be written with a decimal point e.g., .99 cents) : ");
-		System.out.println();
+				
+		double changeDue = input.nextDouble();
 		
-		double totalChangeDue = input.nextDouble();
+		System.out.println();		
 		
-		System.out.println();
+		int numberOfDollars = numberOfDollars(changeDue);
+		int numberOfQuarters = numberOfQuarters(changeDue, numberOfDollars);
+		int numberOfDimes = numberOfDimes(changeDue, numberOfDollars, numberOfQuarters);
+		int numberOfNickels = numberOfNickels(changeDue, numberOfDollars, numberOfQuarters, numberOfDimes);
+		int numberOfPennies = numberOfPennies(changeDue, numberOfDollars, numberOfQuarters, numberOfDimes, numberOfNickels);
 		
-		int calculatedNumberOfQuarters = calculateNumberOfQuarters(totalChangeDue);
-		int calculatedNumberOfDimes = calculateNumberOfDimes(totalChangeDue, calculatedNumberOfQuarters);
-		int calculatedNumberOfNickels = calculateNumberOfNickels(totalChangeDue, calculatedNumberOfQuarters, calculatedNumberOfDimes);
-		int calculatedNumberOfPennies = calculateNumberOfPennies(totalChangeDue, calculatedNumberOfQuarters, calculatedNumberOfDimes, calculatedNumberOfNickels);
-		
-		System.out.println("Quarters: " + calculatedNumberOfQuarters);
-		System.out.println("Dimes: " + calculatedNumberOfDimes);
-		System.out.println("Nickels: " + calculatedNumberOfNickels);
-		System.out.println("Pennies: " + calculatedNumberOfPennies);
+		System.out.println("Dollars: " + numberOfDollars);
+		System.out.println("Quarters: " + numberOfQuarters);
+		System.out.println("Dimes: " + numberOfDimes);
+		System.out.println("Nickels: " + numberOfNickels);
+		System.out.println("Pennies: " + numberOfPennies);
 		
 		input.close();
-		
 	}
 	
-	private static int calculateNumberOfQuarters(double totalChangeDue) {
-		
-		int valueOfQuarter = 25;
-		
-		int calculatedNumberOfQuarters = (int) ((totalChangeDue * 100) / valueOfQuarter);
-		
-		return calculatedNumberOfQuarters;
-		
+	private static int numberOfDollars(double changeDue) 
+	{		
+		int dollar = 100;
+		int numberOfQuarters = (int) ((changeDue*100)/dollar);
+		return numberOfQuarters;		
+	}	
+	private static int numberOfQuarters(double changeDue, int numberOfDollars) 
+	{		//******
+		int dollar = 100;
+		int quarter = 25;
+		int numberOfQuarters = (int) (((changeDue*100)-(numberOfDollars*dollar))/quarter);
+		return numberOfQuarters;		
 	}
 	
-	private static int calculateNumberOfDimes(double totalChangeDue, int calculatedNumberOfQuarters) {
-		
-		int valueOfQuarter = 25;
-		int valueOfDime = 10;
-		
-		int calculatedNumberOfDimes = (int) (((totalChangeDue * 100) - (calculatedNumberOfQuarters * valueOfQuarter)) / valueOfDime);
-		
-		return calculatedNumberOfDimes;
-		
+	private static int numberOfDimes(double changeDue, int numberOfDollars, int numberOfQuarters) 
+	{		
+		int dollar = 100;
+		int quarter = 25;
+		int dime = 10;
+		int numberOfDimes = (int) (((changeDue*100)-(numberOfDollars*dollar)-(numberOfQuarters*quarter))/dime);
+		return numberOfDimes;		
 	}
 
-	private static int calculateNumberOfNickels(double totalChangeDue, int calculatedNumberOfQuarters, int calculatedNumberOfDimes) {
-		
-		int valueOfQuarter = 25;
-		int valueOfDime = 10;
-		int valueOfNickel = 5;
-		
-		int calculatedNumberOfNickels = (int) (((totalChangeDue * 100) - (calculatedNumberOfQuarters * valueOfQuarter) - (calculatedNumberOfDimes * valueOfDime)) / valueOfNickel);
-		
-		return calculatedNumberOfNickels;
-		
+	private static int numberOfNickels(double changeDue, int numberOfDollars, int numberOfQuarters, int numberOfDimes) 
+	{		
+		int dollar = 100;
+		int quarter = 25;
+		int dime = 10;
+		int nickel = 5;		
+		int numberOfNickels = (int) (((changeDue*100)-(numberOfDollars*dollar)-(numberOfQuarters*quarter)-(numberOfDimes*dime))/nickel);
+				return numberOfNickels;		
 	}
 
-	private static int calculateNumberOfPennies(double totalChangeDue, int calculatedNumberOfQuarters, int calculatedNumberOfDimes, int calculatedNumberOfNickels) {
-		
-		int valueOfQuarter = 25;
-		int valueOfDime = 10;
-		int valueOfNickel = 5;
-		int valueOfPenny = 1;
-		
-		int calculatedNumberOfPennies = (int) (((totalChangeDue * 100) - (calculatedNumberOfQuarters * valueOfQuarter) - (calculatedNumberOfDimes * valueOfDime) - (calculatedNumberOfNickels * valueOfNickel)) / valueOfPenny);
-		
-		return calculatedNumberOfPennies;
-		
+	private static int numberOfPennies(double changeDue, int numberOfDollars, int numberOfQuarters, int numberOfDimes, int numberOfNickels) 
+	{		
+		int dollar = 100;
+		int quarter = 25;
+		int dime = 10;
+		int nickel = 5;		
+		int numberOfPennies = (int) (((changeDue*100)-(numberOfDollars*dollar)-(numberOfQuarters*quarter)-(numberOfDimes*dime)-(numberOfNickels*nickel)));
+		return numberOfPennies;		
 	}
-
 }
